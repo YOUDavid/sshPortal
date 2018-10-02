7727268c5241ce248298afd5cffcb04331762a60
YOUDavid <8342705818ydyttt@gmail.com>
2018-10-02 18:37:23 +0800

    finished option 1,2 and 5

--- ssh_portal.sh
+++ ssh_portal.sh
@@ -20,7 +20,7 @@ function find_free_port() {
 # $?: 1 if success, 0 otherwise
 local return_port=$1
 local grep_result=''
-if [ -z ${return_port+x} ]; then
+if [ -z $return_port ]; then
     return_port=49152
 fi
 local telnet_result=`netstat -lnt`
@@ -38,9 +38,9 @@ return 1
 
 function clean_profile(){
 # $1:   name of profile to be cleaned
-    if [ -e $1 ]; then
-        local closingport=`cat $1`
-        [ -z \`fuser ,,$closingport/tcp 2>/dev/null\` ] && fuser $closingport/tcp 2>/dev/null | cut -f1 | xargs -i kill {} 2>/dev/null\ && rm $1
+    if [ -e ".ssh_portal.active.$1" ]; then
+        local closingport=`cat ".ssh_portal.active.$1"`
+        [ -z `fuser ,,$closingport/tcp 2>/dev/null` ] && fuser $closingport/tcp 2>/dev/null | cut -f1 | xargs -i kill {} 2>/dev/null && rm ".ssh_portal.active.$1"
     fi
     return 0
 }
@@ -60,14 +60,16 @@ if [ -e .ssh_portal.data ]; then
 else
     declare -A saved_profiles
 fi
+trap "declare -p saved_profiles > .ssh_portal.data" 0
 while true; do
     echo '0: exit'
     echo '1: ssh connect to a profile'
     echo '2: sshfs mount remote directory'
-    echo '3: create profile'    
+    echo '3: unmount previously mounted directory'
+    echo '4: list active mounted directories'
+    echo '5: create profile'    
     read -p 'Please type a number to continue: ' input_command 
     if [ '0' == "$input_command" ]; then
-        #declare -p saved_profiles > .ssh_portal.data
         break
     elif [ '1' == "$input_command" ]; then
         read -p 'Please enter the profile name: ' profile_name
@@ -76,7 +78,7 @@ while true; do
             continue
         fi
         ###
-        while IFS='|' read user_name server_address firewall_address encrypted_password <<< `echo ${saved_profiles[$profile_name]}`
+        while IFS='|' read profile_name user_name server_address firewall_address encrypted_password <<< `echo ${saved_profiles[$profile_name]}`
         do
             break
         done
@@ -86,16 +88,16 @@ while true; do
         while [ $try_time -gt "0" ]
         do
             read -s -p $'Enter the password: ' input_password
-            encrypted_input=`echo -n $input_password | sha512sum`
-            if [ "$encrypted_input" != "$encrypted_password" ]
-            then 
+            echo ''
+            encrypted_input=`echo -n $input_password | sha512sum | cut -d' ' -f1`
+            if [ "$encrypted_input" != "$encrypted_password" ]; then 
                 echo "Incorrect password!"
                 let try_time=try_time-1        
             else
                 #check port 
                 #isPortUsed=`fuser $freeport/tcp 2>/dev/null | cut -f1`
 
-                if [ -z ".ssh_portal.active.$profile_name" ]
+                if [ ! -e ".ssh_portal.active.$profile_name" ]
                 then
                     #This is the first active profile of the same ones
                     freeport=`find_free_port`
@@ -106,20 +108,20 @@ while true; do
                     
                     #Close the tunnel after no connection is made to the local port and even if the terminal was closed by other means
                     #trap "[ -z \`fuser ,,$freeport/tcp 2>/dev/null\` ] && fuser $freeport/tcp 2>/dev/null | cut -f1 | xargs -i kill {} 2>/dev/null" 0
-                    trap "clean_profile $profile_name" 0
+                    
                     if [ -z firewall_address ]; then
                         sshpass -p $input_password ssh -L $freeport:$server_address:22 -N -o StrictHostKeyChecking=no
                     else
                         sshpass -p $input_password ssh -f $user_name@$firewall_address -L $freeport:$server_address:22 -N -o StrictHostKeyChecking=no
                     fi
                     echo -n $freeport > .ssh_portal.active.$profile_name
+                    
                 else
                     freeport=`cat .ssh_portal.active.$profile_name`
                 fi
-                
+                trap "clean_profile $profile_name" 0
                 sshpass -p $input_password ssh -p $freeport $user_name@localhost -o StrictHostKeyChecking=no
-                
-                exit_status=0
+
                 #Close the tunnel after no connection is made to the local port
                 clean_profile $profile_name
                 break
@@ -127,8 +129,85 @@ while true; do
         done
     elif [ '2' == "$input_command" ]; then
         ###TODO: sshfs mount a profile
-        echo 2
+        
+        read -p 'Please enter the profile name: ' profile_name
+        if [ -z ${saved_profiles[$profile_name]+dummy} ]; then
+            echo "Can't find such profile: $profile_name"
+            continue
+        fi
+        
+        while IFS='|' read profile_name user_name server_address firewall_address encrypted_password <<< `echo ${saved_profiles[$profile_name]}`; do
+            break
+        done
+        
+        exec 200>~/.ssh_portal.mount.$profile_name
+        flock -xn 200 || { echo "Already connected to carbon!!!"; continue; }
+        
+        if [ -d "~/$user_name@$profile_name" ]; then
+            echo "Directory to be mounted already exists!!!"
+            continue
+        else
+            mkdir ~/"$user_name"@"$profile_name"
+        fi
+        
+        try_time="3"
+        while [ $try_time -gt "0" ]; do
+            read -s -p $'Enter the password: ' input_password
+            echo ''
+            encrypted_input=`echo -n $input_password | sha512sum | cut -d' ' -f1`
+            if [ "$encrypted_input" != "$encrypted_password" ]; then 
+                echo "Incorrect password!"
+                let try_time=try_time-'1'        
+            else
+                if [ ! -e ".ssh_portal.active.$profile_name" ]
+                then
+                    #This is the first active profile of the same ones
+                    freeport=`find_free_port`
+                    if [ $? -gt 0 ]; then
+                        echo "Error in finding a free local port!!!"
+                        break
+                    fi
+                    
+                    #Close the tunnel after no connection is made to the local port and even if the terminal was closed by other means
+                    #trap "[ -z \`fuser ,,$freeport/tcp 2>/dev/null\` ] && fuser $freeport/tcp 2>/dev/null | cut -f1 | xargs -i kill {} 2>/dev/null" 0
+                    if [ -z firewall_address ]; then
+                        sshpass -p $input_password ssh -L $freeport:$server_address:22 -N -o StrictHostKeyChecking=no
+                    else
+                        sshpass -p $input_password ssh -f $user_name@$firewall_address -L $freeport:$server_address:22 -N -o StrictHostKeyChecking=no
+                    fi
+                    echo -n $freeport > .ssh_portal.active.$profile_name
+                    
+                else
+                    freeport=`cat .ssh_portal.active.$profile_name`
+                fi
+                trap "clean_profile $profile_name" 0
+                remote_home=`sshpass -p $input_password ssh -p $freeport $user_name@localhost -o StrictHostKeyChecking=no 'echo $HOME'`
+                echo $input_password | sshfs -o password_stdin -p $freeport "$user_name@localhost:$remote_home" "~/$user_name@$profile_name"
+            fi
+        done
     elif [ '3' == "$input_command" ]; then
+        ###TODO: unmount a previously mounted directory
+        read -p 'Please enter the profile name: ' profile_name
+        if [ -z ${saved_profiles[$profile_name]+dummy} ]; then
+            echo "Can't find such profile: $profile_name"
+            continue
+        fi
+        
+        while IFS='|' read profile_name user_name server_address firewall_address encrypted_password <<< `echo ${saved_profiles[$profile_name]}`; do
+            break
+        done
+       
+        if [ -e .ssh_portal.mount.$profile_name ]; then
+            fusermount -u ~/$user_name@$profile_name
+            rm ~/.ssh_portal.mount.$profile_name
+            clean_profile $profile_name
+        else
+            echo "Profile is not mounted!!!"
+        fi
+    elif [ '4' == "$input_command" ]; then
+        ###TODO: list all active mounted directories
+        ls -A .ssh_portal.mount.*
+    elif [ '5' == "$input_command" ]; then
         ###TODO: create profile
         read -p 'Please enter the new profile name: ' profile_name
         while [ ! -z ${saved_profiles[$profile_name]+dummy} ]; do
@@ -147,14 +226,14 @@ while true; do
         done
         read -p 'Please enter the firewall address (empty for not using firewall): ' firewall_address
         read -s -p 'Enter the password: ' encrypted_password
-        encrypted_password=`echo -n $encrypted_password | sha512sum`
+        echo ''
+        encrypted_password=`echo -n $encrypted_password | sha512sum | cut -d' ' -f1`
         saved_profiles[$profile_name]="$profile_name|$user_name|$server_address|$firewall_address|$encrypted_password"
         unset profile_name
         unset user_name
         unset server_address
         unset firewall_address
         unset encrypted_password
-        fi 
         
     else
         echo "Can not understand command: $input_command !!!"
